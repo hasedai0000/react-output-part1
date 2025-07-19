a32988b3e19ea99e78860d62ed61626a9a2426aa
daichi-hasegawa-styleedge <d.hasegawa@styleedge.co.jp>
2025-07-19 21:07:16 +0900

    Todo表示箇所のコンポーネント化

--- src/components/templates/TodoTemplate/TodoTemplate.jsx
+++ src/components/templates/TodoTemplate/TodoTemplate.jsx
@@ -1,7 +1,5 @@
 import { useState } from "react";
-import { faTrashAlt } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-
+import { TodoList } from "../../organisms/TodoList";
 import { INIT_TODO_LIST, INIT_UNIQUE_ID } from "../../../constants/data.js";
 
 import styles from "./style.module.css";
@@ -9,22 +7,13 @@ import styles from "./style.module.css";
 export const TodoTemplate = () => {
   /** Todo List */
   const [originTodoList, setOriginTodoList] = useState(INIT_TODO_LIST);
+
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>Todo List</h1>
       <section className={styles.common}>
         {originTodoList.length > 0 ? (
-          <ul className={styles.list}>
-            {originTodoList.map((todo) => (
-              <li key={todo.id} className={styles.todo}>
-                <span className={styles.task}>{todo.title}</span>
-                <div className={styles.far}>
-                  {/* https://www.digitalocean.com/community/tutorials/how-to-use-font-awesome-5-with-react-ja */}
-                  <FontAwesomeIcon icon={faTrashAlt} size="lg" />
-                </div>
-              </li>
-            ))}
-          </ul>
+          <TodoList todoList={originTodoList} />
         ) : (
           <p className={styles.noList}>リストがありません</p>
         )}
