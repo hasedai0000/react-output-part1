35eb71084c44931348103edefa404a78fb0f37fe
daichi-hasegawa-styleedge <d.hasegawa@styleedge.co.jp>
2025-07-19 21:26:44 +0900

    検索絞り込み機能の実装

--- src/components/templates/TodoTemplate/TodoTemplate.jsx
+++ src/components/templates/TodoTemplate/TodoTemplate.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useMemo } from "react";
 import { TodoList } from "../../organisms/TodoList";
 import { INIT_TODO_LIST, INIT_UNIQUE_ID } from "../../../constants/data.js";
 
@@ -7,13 +7,38 @@ import styles from "./style.module.css";
 export const TodoTemplate = () => {
   /** Todo List */
   const [originTodoList, setOriginTodoList] = useState(INIT_TODO_LIST);
+  /** 検索キーワード */
+  const [searchKeyword, setSearchKeyword] = useState("");
+
+  /** 検索キーワードで絞り込んだTodo List */
+  const filteredTodoList = useMemo(() => {
+    return originTodoList.filter((todo) => {
+      //　検索キーワードに部分一致したTodoだけを一覧表示する
+      const regexp = new RegExp("" + searchKeyword, "i");
+      return todo.title.match(regexp);
+    });
+  }, [originTodoList, searchKeyword]);
+
+  /** action */
+  const handleChangeSearchKeyword = (e) => {
+    setSearchKeyword(e.target.value);
+  };
 
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>Todo List</h1>
       <section className={styles.common}>
-        {originTodoList.length > 0 ? (
-          <TodoList todoList={originTodoList} />
+        <input
+          className={styles.input}
+          type="text"
+          placeholder="検索キーワード"
+          value={searchKeyword}
+          onChange={(e) => handleChangeSearchKeyword(e)}
+        />
+      </section>
+      <section className={styles.common}>
+        {filteredTodoList.length > 0 ? (
+          <TodoList todoList={filteredTodoList} />
         ) : (
           <p className={styles.noList}>リストがありません</p>
         )}
